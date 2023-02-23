54502a854083fa3cb175e2e9c7d31ba44751f8d9
junxian428 <holeyiho@gmail.com>
2023-02-23 15:48:07 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_fastapi_jetson/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_fastapi_jetson
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HTTP Call Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'HTTP Call Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String? _responseData;
+
+  Future<void> _fetchData(String url) async {
+    setState(() {
+      _responseData = null;
+    });
+
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        setState(() {
+          _responseData = response.body;
+        });
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (error) {
+      setState(() {
+        _responseData = 'Error: $error';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (_responseData == null)
+              Text(
+                'Press a button to make an HTTP call:',
+              ),
+            if (_responseData != null)
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      _responseData!,
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                ),
+              ),
+            SizedBox(height: 16.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => _fetchData('http://192.168.10.244:8000/motor_run'),
+                  child: Text('Start Motor'),
+                ),
+                SizedBox(width: 16.0),
+                ElevatedButton(
+                  onPressed: () => _fetchData('http://192.168.10.244:8000/motor_stop'),
+                  child: Text('Stop motor'),
+                ),
+              ],
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () => _fetchData('https://jsonplaceholder.typicode.com/todos/3'),
+              child: Text('Fetch Data 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
